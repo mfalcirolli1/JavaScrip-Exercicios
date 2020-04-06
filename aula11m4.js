62fd5fbfad54a8bc50f03bdb9a92883685da92b3
Matheus Falcirolli <fmatheus8@gmail.com>
2020-04-06 16:11:17 -0300

    New exercise files

---
+++ aula11m4.js
@@ -0,0 +1,20 @@
+var velocidade = 70.5
+console.log(`A velocidade do seu carro é de ${velocidade} Km/h`)
+console.log("Dirija utilizando o cinto de segurança!")
+
+if(velocidade > 70 ){
+    console.log("Reduza a velocidade!")
+}
+else{
+    console.log("Boa velocidade, continue assim.")
+}
+
+
+var pais = "Brasil"
+console.log(`Vivendo em ${pais}: `)
+if(pais == "Brasil"){
+    console.log("Brasileiro!")
+}
+else{
+    console.log("Estrangeiro!")
+}
